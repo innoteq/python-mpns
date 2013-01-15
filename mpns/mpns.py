1e4482a65504865f9222bc8ac981806467dc3e18
Max Arnold <arnold.maxim@gmail.com>
2013-01-15 12:08:55 +0700

    populate HTTP headers

--- mpns/mpns.py
+++ mpns/mpns.py
@@ -11,10 +11,26 @@ class MPNSBase(object):
     DELAY_450S = None
     DELAY_900S = None
 
+    def __init__(self, delay=None):
+        self.delay = delay or self.DELAY_IMMEDIATE
+        self.headers = {
+            'Content-Type': 'text/xml',
+            'Accept': 'application/*',
+            'X-NotificationClass': str(self.delay),
+            }
+
+    def set_target(self, target):
+        self.headers['X-WindowsPhone-Target'] = target
+
+
 class MPNSTile(MPNSBase):
     DELAY_IMMEDIATE = 1
     DELAY_450S = 11
     DELAY_900S = 21
+
+    def __init__(self, *args, **kwargs):
+        super(MPNSTile, self).__init__(*args, **kwargs)
+        self.set_target('tile') # TODO: flip tile
 
 
 class MPNSToast(MPNSBase):
@@ -22,8 +38,16 @@ class MPNSToast(MPNSBase):
     DELAY_450S = 12
     DELAY_900S = 22
 
+    def __init__(self, *args, **kwargs):
+        super(MPNSToast, self).__init__(*args, **kwargs)
+        self.set_target('token')
+
 
 class MPNSRaw(MPNSBase):
     DELAY_IMMEDIATE = 3
     DELAY_450S = 13
     DELAY_900S = 23
+
+    def __init__(self, *args, **kwargs):
+        super(MPNSRaw, self).__init__(*args, **kwargs)
+        self.set_target('raw')
