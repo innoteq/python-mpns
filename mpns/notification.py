c06f22062e9168bc6509cc573670fe02a69907be
Max Arnold <arnold.maxim@gmail.com>
2013-01-17 22:40:28 +0700

    move documentation links from source code to readme

--- mpns/notification.py
+++ mpns/notification.py
@@ -3,13 +3,6 @@ from cStringIO import StringIO
 
 import requests
 
-# MPNS overview: http://msdn.microsoft.com/en-us/library/ff402558%28v=vs.92%29.aspx
-# Protocol specification: http://msdn.microsoft.com/en-us/library/hh202945%28v=vs.92%29.aspx
-# WP8 enhancements: http://msdn.microsoft.com/en-us/library/windowsphone/develop/hh202948%28v=vs.105%29.aspx
-# Error codes: http://msdn.microsoft.com/en-us/library/ff941100%28v=VS.92%29.aspx
-
-# Also see Authenticated Web Service: http://msdn.microsoft.com/en-us/library/ff941099%28v=vs.92%29.aspx
-
 # TODO: test escaping
 
 class MPNSBase(object):
