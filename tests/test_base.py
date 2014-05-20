403bce18763dca71eac75ee05b79d8660daa93b1
stereofuture <brianeatworld@gmail.com>
2014-05-20 00:12:36 -0700

    finished parse_response tests for all scenarios, renamed class to Base_Test

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,11 +1,11 @@
-import unittest
+# import unittest
 import httpretty
 import requests
 
 from mpns import MPNSTile
 
 
-class test_base(unittest.TestCase):
+class Test_Base(object):
 	
 	@httpretty.activate
 	def test_parse_response_200(self):
@@ -88,31 +88,196 @@ class test_base(unittest.TestCase):
 		assert status['notification_status'] == 'Test'
 		assert status['message_id'] == 'Test'
 
-		#####Response Logic#####
-        # if code == 200:
-            # if status['notification_status'] == 'QueueFull':
-                # status['error'] = 'Queue full, try again later'
-                # status['backoff_seconds'] = 60
-        # elif code == 400:
-            # status['error'] = 'Bad Request - invalid payload or subscription URI'
-        # elif code == 401:
-            # status['error'] = 'Unauthorized - invalid token or subscription URI'
-            # status['drop_subscription'] = True
-        # elif code == 404:
-            # status['error'] = 'Not Found - subscription URI is invalid'
-            # status['drop_subscription'] = True
-        # elif code == 405:
-            # status['error'] = 'Invalid Method' # (this should not happen, module uses only POST method)
-        # elif code == 406:
-            # status['error'] = 'Not Acceptable - per-day throttling limit reached'
-            # status['backoff_seconds'] = 24 * 60 * 60
-        # elif code == 412:
-            # status['error'] = 'Precondition Failed - device inactive, try once per-hour'
-            # status['backoff_seconds'] = 60 * 60
-        # elif code == 503:
-            # status['error'] = 'Service Unavailable - try again later'
-            # status['backoff_seconds'] = 60
-        # else:
-            # status['error'] = 'Unexpected status'
+	@httpretty.activate
+	def test_parse_response_401(self):
+		#Mocking response headers
+		test_tile_URI = "http://testuri.com/"
+		httpretty.register_uri(httpretty.POST, 
+								test_tile_URI,
+								content_type='text/json',
+								adding_headers={
+									"x-deviceconnectionstatus":"Test",
+									"x-notificationstatus":"Test",
+									"x-subscriptionstatus":"Test",
+									"x-messageid":"Test"},
+								status=401)
 
-        # return status
+		#Creating MPNS object, posting request and submitting response to parse_response
+		test_tile = MPNSTile()		
+		response = requests.post(test_tile_URI)
+		status = test_tile.parse_response(response)
+
+		#Checking status code, error message and ensuring other headers passed without unaltered
+		assert status['http_status_code'] == 401
+		assert status['error'] == 'Unauthorized - invalid token or subscription URI'
+		assert status['drop_subscription'] == True
+		assert status['device_connection_status'] == 'Test'
+		assert status['subscription_status'] == 'Test'
+		assert status['notification_status'] == 'Test'
+		assert status['message_id'] == 'Test'
+
+	@httpretty.activate
+	def test_parse_response_404(self):
+		#Mocking response headers
+		test_tile_URI = "http://testuri.com/"
+		httpretty.register_uri(httpretty.POST, 
+								test_tile_URI,
+								content_type='text/json',
+								adding_headers={
+									"x-deviceconnectionstatus":"Test",
+									"x-notificationstatus":"Test",
+									"x-subscriptionstatus":"Test",
+									"x-messageid":"Test"},
+								status=404)
+
+		#Creating MPNS object, posting request and submitting response to parse_response
+		test_tile = MPNSTile()		
+		response = requests.post(test_tile_URI)
+		status = test_tile.parse_response(response)
+
+		#Checking status code, error message and ensuring other headers passed without unaltered
+		assert status['http_status_code'] == 404
+		assert status['error'] == 'Not Found - subscription URI is invalid'
+		assert status['drop_subscription'] == True
+		assert status['device_connection_status'] == 'Test'
+		assert status['subscription_status'] == 'Test'
+		assert status['notification_status'] == 'Test'
+		assert status['message_id'] == 'Test'
+
+	@httpretty.activate
+	def test_parse_response_405(self):
+		#Mocking response headers
+		test_tile_URI = "http://testuri.com/"
+		httpretty.register_uri(httpretty.POST, 
+								test_tile_URI,
+								content_type='text/json',
+								adding_headers={
+									"x-deviceconnectionstatus":"Test",
+									"x-notificationstatus":"Test",
+									"x-subscriptionstatus":"Test",
+									"x-messageid":"Test"},
+								status=405)
+
+		#Creating MPNS object, posting request and submitting response to parse_response
+		test_tile = MPNSTile()		
+		response = requests.post(test_tile_URI)
+		status = test_tile.parse_response(response)
+
+		#Checking status code, error message and ensuring other headers passed without unaltered
+		assert status['http_status_code'] == 405
+		assert status['error'] == 'Invalid Method'
+		assert status['device_connection_status'] == 'Test'
+		assert status['subscription_status'] == 'Test'
+		assert status['notification_status'] == 'Test'
+		assert status['message_id'] == 'Test'
+		
+	@httpretty.activate
+	def test_parse_response_406(self):
+		#Mocking response headers
+		test_tile_URI = "http://testuri.com/"
+		httpretty.register_uri(httpretty.POST, 
+								test_tile_URI,
+								content_type='text/json',
+								adding_headers={
+									"x-deviceconnectionstatus":"Test",
+									"x-notificationstatus":"Test",
+									"x-subscriptionstatus":"Test",
+									"x-messageid":"Test"},
+								status=406)
+
+		#Creating MPNS object, posting request and submitting response to parse_response
+		test_tile = MPNSTile()		
+		response = requests.post(test_tile_URI)
+		status = test_tile.parse_response(response)
+
+		#Checking status code, error message and ensuring other headers passed without unaltered
+		assert status['http_status_code'] == 406
+		assert status['error'] == 'Not Acceptable - per-day throttling limit reached'
+		assert status['backoff_seconds'] == 24*60*60
+		assert status['device_connection_status'] == 'Test'
+		assert status['subscription_status'] == 'Test'
+		assert status['notification_status'] == 'Test'
+		assert status['message_id'] == 'Test'
+
+	@httpretty.activate
+	def test_parse_response_412(self):
+		#Mocking response headers
+		test_tile_URI = "http://testuri.com/"
+		httpretty.register_uri(httpretty.POST, 
+								test_tile_URI,
+								content_type='text/json',
+								adding_headers={
+									"x-deviceconnectionstatus":"Test",
+									"x-notificationstatus":"Test",
+									"x-subscriptionstatus":"Test",
+									"x-messageid":"Test"},
+								status=412)
+
+		#Creating MPNS object, posting request and submitting response to parse_response
+		test_tile = MPNSTile()		
+		response = requests.post(test_tile_URI)
+		status = test_tile.parse_response(response)
+
+		#Checking status code, error message and ensuring other headers passed without unaltered
+		assert status['http_status_code'] == 412
+		assert status['error'] == 'Precondition Failed - device inactive, try once per-hour'
+		assert status['backoff_seconds'] == 60*60
+		assert status['device_connection_status'] == 'Test'
+		assert status['subscription_status'] == 'Test'
+		assert status['notification_status'] == 'Test'
+		assert status['message_id'] == 'Test'
+
+	@httpretty.activate
+	def test_parse_response_503(self):
+		#Mocking response headers
+		test_tile_URI = "http://testuri.com/"
+		httpretty.register_uri(httpretty.POST, 
+								test_tile_URI,
+								content_type='text/json',
+								adding_headers={
+									"x-deviceconnectionstatus":"Test",
+									"x-notificationstatus":"Test",
+									"x-subscriptionstatus":"Test",
+									"x-messageid":"Test"},
+								status=503)
+
+		#Creating MPNS object, posting request and submitting response to parse_response
+		test_tile = MPNSTile()		
+		response = requests.post(test_tile_URI)
+		status = test_tile.parse_response(response)
+
+		#Checking status code, error message and ensuring other headers passed without unaltered
+		assert status['http_status_code'] == 503
+		assert status['error'] == 'Service Unavailable - try again later'
+		assert status['backoff_seconds'] == 60
+		assert status['device_connection_status'] == 'Test'
+		assert status['subscription_status'] == 'Test'
+		assert status['notification_status'] == 'Test'
+		assert status['message_id'] == 'Test'
+		
+	@httpretty.activate
+	def test_parse_response_505(self):
+		#Mocking response headers
+		test_tile_URI = "http://testuri.com/"
+		httpretty.register_uri(httpretty.POST, 
+								test_tile_URI,
+								content_type='text/json',
+								adding_headers={
+									"x-deviceconnectionstatus":"Test",
+									"x-notificationstatus":"Test",
+									"x-subscriptionstatus":"Test",
+									"x-messageid":"Test"},
+								status=505)
+
+		#Creating MPNS object, posting request and submitting response to parse_response
+		test_tile = MPNSTile()		
+		response = requests.post(test_tile_URI)
+		status = test_tile.parse_response(response)
+
+		#Checking status code, error message and ensuring other headers passed without unaltered
+		assert status['http_status_code'] == 505
+		assert status['error'] == 'Unexpected status'
+		assert status['device_connection_status'] == 'Test'
+		assert status['subscription_status'] == 'Test'
+		assert status['notification_status'] == 'Test'
+		assert status['message_id'] == 'Test'
