f924d014eb612a94562930976d741b01ab99fbea
Max Arnold <arnold.maxim@gmail.com>
2015-01-06 10:59:28 +0700

    bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.core import setup
 
 __author__ = 'Max Arnold <arnold.maxim@gmail.com>'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 setup(
     name='python-mpns',
